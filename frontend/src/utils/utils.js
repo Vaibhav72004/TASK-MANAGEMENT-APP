89451b6c26ecf4be7c45347b2337a2b4a21d1f67
Vaibhav72004 <vaibhavmishra72004@gmail.com>
2025-11-12 01:31:20 +0530

    fix: update API base URL in environment and utils for consistency

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,2 @@
-// export const BACKEND_URL = "https://task-management-application-wjvt.onrender.com/api";
-
-
-export const BACKEND_URL = "http://localhost:5000/api";
+export const BACKEND_URL = "https://task-management-app-hmaz.onrender.com/api";
 
